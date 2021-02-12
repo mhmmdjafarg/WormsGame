b852b4d651c14de8c86699f39e82ffae7fd2a76b
Randy Zakya Suchrady <impper1@gmail.com>
2021-02-12 16:06:53 +0700

    method isenemythrowable

--- src/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ src/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -176,18 +176,28 @@ public class Bot {
         }
     }
 
-    private boolean isEnemyShootable(Position enemy_position) { //randy
-        /** cek di sekitar musuh ada temen atau ngga **/
+    private boolean isEnemyThrowable(Position enemy_position , String utility) { //randy
+        /** cek di sekitar musuh ada temen atau ngga
+         * utility bisa berupa "snow" atau "banana"
+         */
         for (int i = 0; i < 3; ++i) {
             if (gameState.myPlayer.worms[i].health != 0) {
                 /** kalau ga mati berarti ada kemungkinan deket musuh **/
                 int deltax = gameState.myPlayer.worms[i].position.x - enemy_position.x;
                 int deltay = gameState.myPlayer.worms[i].position.y - enemy_position.y;
-                if (Math.sqrt(Math.pow(deltax, 2) + Math.pow(deltay, 2)) <= 2) {
+                int radius;
+                if (utility == "banana"){
+                    radius = 2;
+                }else if (utility == "snow") {
+                    radius = 1;
+                }
+                if (Math.sqrt(Math.pow(deltax, 2) + Math.pow(deltay, 2)) <= radius) {
                     return false;
                 }
             }
         }
         return true;
     }
+
+
 }
